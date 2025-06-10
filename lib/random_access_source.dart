25816cab457284c068f1043b69f2376a7e69abf8
Mgen <m@mgenware.com>
2025-06-10 11:50:25 +0800

    Remove undocumented binary reader

--- lib/random_access_source.dart
+++ lib/random_access_source.dart
@@ -1,7 +1,6 @@
 /// Shared interfaces for random access data.
 library;
 
-export 'src/random_access_source.dart';
 export 'src/bytes_ra_source.dart';
 export 'src/file_ra_source.dart';
-export 'src/random_access_binary_reader.dart';
+export 'src/random_access_source.dart';
